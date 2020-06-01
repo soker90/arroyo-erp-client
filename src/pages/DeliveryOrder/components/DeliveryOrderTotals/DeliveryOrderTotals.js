408f3c420f5d22ae43efb714c08a36261430c86f
Eduardo Parra <edue17@gmail.com>
2020-06-01 21:57:41 +0200

    added taxBase to delivery order

--- src/pages/DeliveryOrder/components/DeliveryOrderTotals/DeliveryOrderTotals.js
+++ src/pages/DeliveryOrder/components/DeliveryOrderTotals/DeliveryOrderTotals.js
@@ -7,19 +7,24 @@ import {
 import { ItemCard } from 'components';
 import { format } from 'utils';
 
-const DeliveryOrderTotals = ({ iva, re, total }) => (
+const DeliveryOrderTotals = ({
+  iva, re, total, taxBase,
+}) => (
   <Card>
     <CardHeader title="Totales" />
     <Divider />
     <CardContent>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={3}>
+          <ItemCard label="Base imponible" value={format.euro(taxBase)} />
+        </Grid>
+        <Grid item xs={12} md={3}>
           <ItemCard label="IVA" value={format.euro(iva)} />
         </Grid>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={3}>
           <ItemCard label="RE" value={format.euro(re)} />
         </Grid>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={3}>
           <ItemCard label="TOTAL" value={format.euro(total)} />
         </Grid>
       </Grid>
@@ -31,6 +36,7 @@ DeliveryOrderTotals.propTypes = {
   iva: PropTypes.number.isRequired,
   re: PropTypes.number.isRequired,
   total: PropTypes.number.isRequired,
+  taxBase: PropTypes.number.isRequired,
 };
 
 DeliveryOrderTotals.displayName = 'DeliveryOrderTotals';
