a4143ccd03c54f6e6c7f54d76ee9987fa42687ef
Eduardo Parra <edue17@gmail.com>
2020-11-01 13:31:18 +0100

    Download book dfile

--- src/pages/Book/components/Header/index.js
+++ src/pages/Book/components/Header/index.js
@@ -2,32 +2,46 @@ import { memo } from 'react';
 import PropTypes from 'prop-types';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import GetAppIcon from '@material-ui/icons/GetApp';
 
 import { Header } from 'components';
 import { NavLink } from 'react-router-dom';
+import { downloadFile } from 'utils';
 
-const HeaderBook = ({ year }) => (
-  <Header
-    title='Libro'
-    description={`Libro contable ${year}`}
-    buttons={[
-      {
-        component: NavLink,
-        to: `/app/libro/${year - 1}`,
-        Icon: SkipPreviousIcon,
-        label: `${year - 1}`,
-        variant: 'outlined',
-      },
-      {
-        component: NavLink,
-        to: `/app/libro/${year + 1}`,
-        Icon: SkipNextIcon,
-        label: `${year + 1}`,
-        variant: 'outlined',
-      },
-    ]}
-  />
-);
+const HeaderBook = ({ year }) => {
+  const _handleClickDownload = () => {
+    downloadFile('http://localhost:3008/invoices/export/2020', `Libro ${year}.ods`);
+  };
+
+  return (
+    <Header
+      title='Libro'
+      description={`Libro contable ${year}`}
+      buttons={[
+        {
+          onClick: _handleClickDownload,
+          Icon: GetAppIcon,
+          label: 'Descargar',
+          variant: 'contained',
+        },
+        {
+          component: NavLink,
+          to: `/app/libro/${year - 1}`,
+          Icon: SkipPreviousIcon,
+          label: `${year - 1}`,
+          variant: 'outlined',
+        },
+        {
+          component: NavLink,
+          to: `/app/libro/${year + 1}`,
+          Icon: SkipNextIcon,
+          label: `${year + 1}`,
+          variant: 'outlined',
+        },
+      ]}
+    />
+  );
+};
 
 HeaderBook.propTypes = {
   year: PropTypes.number.isRequired,
---
+++ src/utils/files.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+/**
+ * Download file from a url
+ * @param {String} url
+ * @param {String} name
+ * @return {Promise}
+ */
+export const downloadFile = (url, name = null) => axios({
+  url,
+  method: 'GET',
+  responseType: 'blob', // important
+}).then(response => {
+  const urlCompose = window.URL.createObjectURL(new Blob([response.data]));
+  const link = document.createElement('a');
+  link.href = urlCompose;
+  link.setAttribute(
+    'download',
+    name || response.headers.filename,
+  );
+  document.body.appendChild(link);
+  link.click();
+});
