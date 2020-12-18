fad5d4ecd928befff15030564d33d8e0d7b903b4
Eduardo Parra <edue17@gmail.com>
2020-12-18 21:16:26 +0100

    fix scroll tables

--- src/components/TableMaterial/TableMaterial.styles.js
+++ src/components/TableMaterial/TableMaterial.styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core';
 export const useStyles = makeStyles(theme => ({
   root: {
     width: 'auto',
+    overflowX: 'visible',
   },
   tooltip: {
     marginRight: theme.spacing(1.5),
