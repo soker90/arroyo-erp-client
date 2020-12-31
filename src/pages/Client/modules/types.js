a0f210d4b6c396db1c7a1ff7f06aebd07930af20
Eduardo Parra <edue17@gmail.com>
2020-12-31 16:03:35 +0100

    fix eslint

--- src/pages/Client/modules/types.js
+++ src/pages/Client/modules/types.js
@@ -4,4 +4,3 @@ export const GET_CLIENT = requestActions('@client/GET_CLIENT');
 export const EDIT_CLIENT = requestActions('@client/EDIT_CLIENT');
 
 export const GET_CLIENT_INVOICES = requestActions('@client/GET_CLIENT_INVOICES');
-
