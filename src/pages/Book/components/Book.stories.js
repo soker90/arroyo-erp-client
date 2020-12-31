9fc65dc221e25c23a206248b513d46e4cddad8ab
Eduardo Parra <edue17@gmail.com>
2020-12-31 16:55:25 +0100

    fix stories

--- src/pages/Book/components/Book.stories.js
+++ src/pages/Book/components/Book.stories.js
@@ -3,14 +3,15 @@ import { action } from '@storybook/addon-actions';
 import MomentUtils from '@date-io/moment';
 import { ReduxProvider } from 'story';
 import RoutesWrapper from 'story/RoutesWrapper';
-import { story as Book } from './Book';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { story as Book } from './Book';
+import { story as Product } from '../../Product/components/Product';
 
 export default {
   title: 'Rutas/Libro',
   parameters: {
     component: Book,
-    componentSubtitle: 'Vista'
+    componentSubtitle: 'Vista',
   },
   decorators: [storyFn => (
     <ReduxProvider>
@@ -20,8 +21,8 @@ export default {
         </RoutesWrapper>
       </MuiPickersUtilsProvider>
     </ReduxProvider>
-  )
-  ]
+  ),
+  ],
 };
 
 /**
@@ -40,11 +41,12 @@ const BookStory = () => (
           concept: 'C. mercaderias',
           dateInvoice: 1595101920000,
           nInvoice: '22',
-          nOrder: 5
-        }
+          nOrder: 5,
+        },
       ]
     }
     getInvoices={action('getInvoices')}
+    match={{ params: { id: 'ggggg7777' } }}
   />
 );
 
