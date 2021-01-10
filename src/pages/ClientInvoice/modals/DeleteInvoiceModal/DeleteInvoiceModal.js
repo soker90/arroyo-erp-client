bb9bb82305eae78fcce3b4690906fc919dd2a73f
Eduardo Parra <edue17@gmail.com>
2021-01-10 12:52:48 +0100

    fix callback

--- src/pages/ClientInvoice/modals/DeleteInvoiceModal/DeleteInvoiceModal.js
+++ src/pages/ClientInvoice/modals/DeleteInvoiceModal/DeleteInvoiceModal.js
@@ -1,19 +1,24 @@
 import { memo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { ConfirmModal } from 'components';
+import { useHistory } from 'react-router';
 
 const DeleteInvoiceModal = ({
   deleteClientInvoice,
   id,
   setShow,
+  client,
   ...rest
 }) => {
+  const history = useHistory();
   const _close = useCallback(() => {
     setShow(false);
   }, [setShow]);
 
   const _handleSend = () => {
-    deleteClientInvoice(id);
+    deleteClientInvoice(id, () => {
+      history.replace(`/app/clientes/${client}`);
+    });
     _close();
   };
 
@@ -46,6 +51,7 @@ DeleteInvoiceModal.propTypes = {
   id: PropTypes.string,
   deleteClientInvoice: PropTypes.func.isRequired,
   show: PropTypes.bool.isRequired,
+  client: PropTypes.string.isRequired,
 };
 
 DeleteInvoiceModal.displayName = 'DeleteProductModal';
--- src/pages/ClientInvoice/modals/DeleteInvoiceModal/DeleteInvoiceModalContainer.js
+++ src/pages/ClientInvoice/modals/DeleteInvoiceModal/DeleteInvoiceModalContainer.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux';
 import DeleteInvoiceModal from './DeleteInvoiceModal';
 import { deleteClientInvoice } from '../../modules/actions';
 
-const mapStateToProps = ({ clientInvoice: { _id } }) => ({
+const mapStateToProps = ({ clientInvoice: { _id, client } }) => ({
   id: _id,
+  client,
 });
 
 const mapDispatchToProps = {
