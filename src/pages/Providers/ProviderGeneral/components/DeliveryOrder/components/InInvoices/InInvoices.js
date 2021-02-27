837915e5dd830ad94d8e6bea2b8f360d76859b00
Eduardo Parra <edue17@gmail.com>
2021-02-27 20:59:11 +0100

    Añadida búsqueda de albaranes por canal

--- src/pages/Providers/ProviderGeneral/components/DeliveryOrder/components/InInvoices/InInvoices.js
+++ src/pages/Providers/ProviderGeneral/components/DeliveryOrder/components/InInvoices/InInvoices.js
@@ -1,23 +1,42 @@
-import { memo } from 'react';
+import {
+  memo, useCallback, useEffect, useMemo, useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 
-import { TableMaterial, TextEuro } from 'components';
+import { InputForm, TableMaterial, TextEuro } from 'components';
 import { format } from 'utils';
 import { BASE_PATH } from 'constants/common';
+import { Card, Grid } from '@material-ui/core';
+import { useDebounce } from 'hooks';
 import { useStyles } from './InInvoices.styles';
 
 const InInvoices = ({
   deliveryOrders: {
     data,
     count,
+    hasCanal,
   },
   idProvider,
   getDeliveryOrders,
 }) => {
+  const [canal, setCanal] = useState('');
   const classes = useStyles();
-  const _refresh = ({
+  const debounce = useDebounce();
+
+  useEffect(() => {
+    debounce(
+      () => {
+        getDeliveryOrders({
+          provider: idProvider,
+          canal,
+        });
+      }, 500,
+    );
+  }, [canal]);
+
+  const _refresh = useCallback(({
     offset,
     limit,
   }) => {
@@ -26,39 +45,62 @@ const InInvoices = ({
       offset,
       limit,
     });
-  };
+  }, [idProvider]);
+
+  const _handleSearch = useCallback(({
+    target: {
+      value,
+    },
+  }) => {
+    setCanal(value);
+  }, [setCanal]);
+
+  const _renderSearch = () => (
+    <Grid container spacing={2} className={classes.search}>
+      <Grid item md={10} />
+      <InputForm label='Canal' size={2} value={canal} onChange={_handleSearch} />
+    </Grid>
+  );
+
+  const columns = useMemo(() => [
+    {
+      title: 'Fecha',
+      render: ({ date }) => format.date(date),
+    },
+    {
+      title: 'Total',
+      // eslint-disable-next-line react/prop-types
+      render: ({ total }) => <TextEuro num={total} />,
+    },
+    {
+      title: 'Nota',
+      field: 'note',
+    },
+  ], []);
+
+  const actions = useMemo(() => [
+    {
+      icon: VisibilityIcon,
+      tooltip: 'Ver',
+      component: Link,
+      to: ({ _id }) => `${BASE_PATH}/albaranes/${_id}`,
+    },
+  ], []);
 
   return (
-    <TableMaterial
-      className={classes.table}
-      columns={[
-        {
-          title: 'Fecha',
-          render: ({ date }) => format.date(date),
-        },
-        {
-          title: 'Total',
-          // eslint-disable-next-line react/prop-types
-          render: ({ total }) => <TextEuro num={total} />,
-        },
-        {
-          title: 'Nota',
-          field: 'note',
-        },
-      ]}
-      data={data}
-      count={count}
-      title='Albaranes en factura'
-      actions={[
-        {
-          icon: VisibilityIcon,
-          tooltip: 'Ver',
-          component: Link,
-          to: ({ _id }) => `${BASE_PATH}/albaranes/${_id}`,
-        },
-      ]}
-      refresh={_refresh}
-    />
+    <Card>
+      {hasCanal && _renderSearch()}
+      <TableMaterial
+        className={classes.table}
+        columns={columns}
+        data={data}
+        count={count}
+        title='Albaranes en factura'
+        actions={actions}
+        refresh={_refresh}
+        withCard={false}
+      />
+    </Card>
   );
 };
 
@@ -66,7 +108,7 @@ InInvoices.propTypes = {
   deliveryOrders: PropTypes.shape({
     data: PropTypes.array,
     count: PropTypes.number,
-    note: PropTypes.string,
+    hasCanal: PropTypes.bool,
   }).isRequired,
   getDeliveryOrders: PropTypes.func.isRequired,
   idProvider: PropTypes.string.isRequired,
--- src/pages/Providers/ProviderGeneral/components/DeliveryOrder/components/InInvoices/InInvoices.styles.js
+++ src/pages/Providers/ProviderGeneral/components/DeliveryOrder/components/InInvoices/InInvoices.styles.js
@@ -4,4 +4,9 @@ export const useStyles = makeStyles(theme => ({
   table: {
     marginTop: theme.spacing(2),
   },
+  search: {
+    marginTop: theme.spacing(2),
+    paddingRight: theme.spacing(3),
+    marginBottom: -theme.spacing(10),
+  },
 }));
