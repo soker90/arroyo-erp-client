fb599c027eb52002f08af0cce29837e8c61e55e6
Eduardo Parra <edue17@gmail.com>
2021-12-19 16:41:46 +0100

    upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|mdx)'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs',
-    '@storybook/addon-docs'
-  ],
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs', '@storybook/addon-docs'],
+  core: {
+    builder: "webpack5"
+  }
 };
