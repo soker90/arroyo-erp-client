1566487e3a39beadb54f4547f33cc02c3003b541
Eduardo Parra <edue17@gmail.com>
2022-01-08 17:13:24 +0100

    add download book button

--- src/pages/ClientBook/components/Header/index.js
+++ src/pages/ClientBook/components/Header/index.js
@@ -4,33 +4,47 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 
 import { Header } from 'components';
 import { NavLink } from 'react-router-dom';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import { downloadFile } from '../../../../utils';
 
-const HeaderBook = ({ year }) => (
-  <Header
-    title='Libro'
-    description={`Libro clientes ${year}`}
-    routes={[{
-      link: '/app/clientes/listado',
-      title: 'Clientes',
-    }]}
-    buttons={[
-      {
-        component: NavLink,
-        to: `/app/clientes/libro/${year - 1}`,
-        Icon: SkipPreviousIcon,
-        label: `${year - 1}`,
-        variant: 'outlined',
-      },
-      {
-        component: NavLink,
-        to: `/app/clientes/libro/${year + 1}`,
-        Icon: SkipNextIcon,
-        label: `${year + 1}`,
-        variant: 'outlined',
-      },
-    ]}
-  />
-);
+const HeaderBook = ({ year }) => {
+  const _handleClickDownload = () => {
+    downloadFile(`client/invoices/export?year=${year}`, `Libro ${year}.ods`);
+  };
+
+  return (
+    <Header
+      title='Libro'
+      description={`Libro clientes ${year}`}
+      routes={[{
+        link: '/app/clientes/listado',
+        title: 'Clientes',
+      }]}
+      buttons={[
+        {
+          onClick: _handleClickDownload,
+          Icon: GetAppIcon,
+          label: 'Descargar',
+          variant: 'contained',
+        },
+        {
+          component: NavLink,
+          to: `/app/clientes/libro/${year - 1}`,
+          Icon: SkipPreviousIcon,
+          label: `${year - 1}`,
+          variant: 'outlined',
+        },
+        {
+          component: NavLink,
+          to: `/app/clientes/libro/${year + 1}`,
+          Icon: SkipNextIcon,
+          label: `${year + 1}`,
+          variant: 'outlined',
+        },
+      ]}
+    />
+  );
+};
 
 HeaderBook.propTypes = {
   year: PropTypes.number.isRequired,
