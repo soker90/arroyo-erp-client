16d70eb5a92e3eeb55a1446f02e3edc145a02f1d
Eduardo Parra <edue17@gmail.com>
2022-05-07 13:23:32 +0200

    update react 18 (#636)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 import 'nprogress/nprogress.css';
 import 'moment/locale/es';
 
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { SettingsProvider } from 'context/SettingsContext';
 import { configureStore } from 'store';
@@ -12,13 +12,14 @@ import App from 'App';
 const store = configureStore();
 const settings = restoreSettings();
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <SettingsProvider settings={settings}>
       <App />
     </SettingsProvider>
-  </Provider>,
-  document.getElementById('root'),
+  </Provider>
 );
 
 // TODO: Activar mas adelante y configurar notificaciones de actualizaciones
