a932bb7e007f0c85aa616ace0265c7cb659d9ddd
Eduardo Parra <edue17@gmail.com>
2022-10-23 13:21:29 +0200

    fix token undefined with mocks

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,8 +10,11 @@ axios.defaults.baseURL = VITE_API_HOST
 axios.interceptors.response.use(
   response => {
     const { token } = response.headers
-    window.localStorage.setItem(ARROYO_TOKEN, token)
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`
+
+    if (token !== undefined) {
+      window.localStorage.setItem(ARROYO_TOKEN, token)
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`
+    }
     return response
   }
 )
