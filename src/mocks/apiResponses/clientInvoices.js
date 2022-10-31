f7f17271f6bcf9c64766f09b613755ce3d792231
Eduardo Parra <edue17@gmail.com>
2022-10-31 14:41:52 +0100

    Fix lint

--- src/mocks/apiResponses/clientInvoices.js
+++ src/mocks/apiResponses/clientInvoices.js
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { rangeFill } from 'utils'
-import { CONCEPT, TYPE_CLIENT_PAYMENT } from '../../constants'
+import { TYPE_CLIENT_PAYMENT } from '../../constants'
 
 const generateClientBilling = () => ({
   _id: faker.database.mongodbObjectId(),
