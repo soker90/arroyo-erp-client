c411f7290d60bfeefa8d46c9c9b3e69b4e3bd7c1
Eduardo Parra <edue17@gmail.com>
2022-12-16 22:27:40 +0100

    change babel by swc

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 
 import { version } from './package.json'
