35c7575de5b631074f6abb3f3c5c14cc5e0c4f02
Eduardo Parra <eduparra90@gmail.com>
2024-07-13 13:59:48 +0200

    switch to tailwind

--- src/components/Forms/SwitchForm/SwitchForm.jsx
+++ src/components/Forms/SwitchForm/SwitchForm.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
-import { FormControlLabel, Switch } from '@mui/material'
 import { Grid } from 'components'
+import { cn } from 'utils'
 
 const SwitchForm = ({
   size = 6,
@@ -12,16 +12,18 @@ const SwitchForm = ({
     item
     md={size}
     xs={12}
+    className='flex'
   >
-    <FormControlLabel
-      control={(
-        <Switch
-          color={color}
-          {...rest}
-        />
-      )}
-      label={label}
-    />
+    <label className='inline-flex items-center cursor-pointer'>
+      <input type='checkbox' value='' className='sr-only peer' {...rest} />
+      <div
+        className={cn("relative w-11 h-6 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300  after:rounded-full after:h-5 after:w-5 after:transition-all",
+          color ? `peer-checked:bg-${color}` : 'peer-checked:bg-primary'
+        )}
+      />
+      <span className='ms-3 text-sm font-medium text-gray-900 dark:text-gray-300'>{label}</span>
+    </label>
+
   </Grid>
 )
 
