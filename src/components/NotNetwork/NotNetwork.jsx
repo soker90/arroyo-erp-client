1f956b99322d0f65c28c32da9be0d9f9637c0f6f
Eduardo Parra <eduparra90@gmail.com>
2024-08-28 20:19:51 +0200

    change

--- src/components/NotNetwork/NotNetwork.jsx
+++ src/components/NotNetwork/NotNetwork.jsx
@@ -1,66 +1,61 @@
 import { Button, Typography } from 'components'
-import { useStyles } from './NotNetwork.styles'
 
-const NotNetwork = () => {
-  const classes = useStyles()
-
-  return (
-    <div
-      className={classes.root}
+const NotNetwork = () => (
+  <div
+    className='p-6 pt[10vh] flex flex-col items-center'
+  >
+    <Typography
+      align='center'
+      variant='h1'
     >
-      <Typography
-        align='center'
-        variant='h1'
-      >
-        Sin conexión
-      </Typography>
-      <Typography
-        align='center'
-        variant='subtitle2'
-        className={classes.subtitle}
-      >
-        ¡Oh, no! No podemos conectar con el Backend.
-      </Typography>
-      <Typography
-        align='center'
-        variant='subtitle2'
-      >
-        1. Comprueba tu conexion a Internet.
-      </Typography>
-      <Typography
-        align='center'
-        variant='subtitle2'
-      >
-        2. Comprueba la conexion a la VPN.
-      </Typography>
-      <Typography
-        align='center'
-        variant='subtitle2'
-      >
-        3. Pulsa Ctrl + shift + R para refrescar.
-      </Typography>
-      <Typography
-        align='center'
-        variant='subtitle2'
-      >
-        4. Pregunta a Backend.
-      </Typography>
-      <div className={classes.imageContainer}>
-        <img
-          alt='image_error'
-          className={classes.image}
-          src='/static/images/error.svg'
-        />
-      </div>
-      <div className={classes.buttonContainer}>
-        <Button
-          onClick={() => window.location.reload()}
-        >
-          Refrescar
-        </Button>
-      </div>
+      Sin conexión
+    </Typography>
+    <Typography
+      align='center'
+      variant='subtitle2'
+      className='mt-2 mb-6'
+    >
+      ¡Oh, no! No podemos conectar con el Backend.
+    </Typography>
+    <Typography
+      align='center'
+      variant='subtitle2'
+    >
+      1. Comprueba tu conexion a Internet.
+    </Typography>
+    <Typography
+      align='center'
+      variant='subtitle2'
+    >
+      2. Comprueba la conexion a la VPN.
+    </Typography>
+    <Typography
+      align='center'
+      variant='subtitle2'
+    >
+      3. Pulsa Ctrl + shift + R para refrescar.
+    </Typography>
+    <Typography
+      align='center'
+      variant='subtitle2'
+    >
+      4. Pregunta a Backend.
+    </Typography>
+    <div className='mt-12 flex justify-center'>
+      <img
+        alt='image_error'
+        className='m-w-full w-[560px] m-h-[300px] h-auto'
+        src='/static/images/error.svg'
+      />
     </div>
-  )
-}
+    <div className='mt-12 flex justify-center'>
+      <Button
+        onClick={() => window.location.reload()}
+      >
+        Refrescar
+      </Button>
+    </div>
+  </div>
+)
 
 export default NotNetwork
