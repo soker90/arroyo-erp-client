0efdb93879c394f03e3ea00d6b5a3080ab2c6964
Eduardo Parra <eduparra90@gmail.com>
2024-09-03 12:17:31 +0200

    change drawer

--- src/layouts/DashboardLayout/NavBar/index.jsx
+++ src/layouts/DashboardLayout/NavBar/index.jsx
@@ -1,45 +1,29 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable react/prop-types */
-import { useEffect } from 'react'
-import { matchPath, useLocation } from 'react-router'
-import { Link as RouterLink } from 'react-router-dom'
-import PerfectScrollbar from 'react-perfect-scrollbar'
 import PropTypes from 'prop-types'
-import {
-  Box, Drawer, Hidden, List, ListSubheader, Typography
-} from '@mui/material'
-import makeStyles from '@mui/styles/makeStyles'
 import uniqId from 'uniqid'
+import { Link as RouterLink } from 'react-router-dom'
+import { Menu } from 'lucide-react'
+import { matchPath, useLocation } from 'react-router'
+import { useState, useEffect } from 'react'
 
 import Logo from 'components/Logo'
-import { navConfig } from 'layouts/DashboardLayout/NavBar/navConfig'
+import { Sheet, SheetContent, SheetTrigger, ScrollArea, Button, Typography } from 'components'
 import { format } from 'utils'
+
+import { navConfig } from './navConfig'
 import NavItem from './NavItem'
 
-function renderNavItems ({
-  items,
-  ...rest
-}) {
+function renderNavItems ({ items, ...rest }) {
   return (
-    <List disablePadding>
+    <ul className='space-y-1'>
       {items.reduce(
-        (acc, item) => reduceChildRoutes({
-          acc,
-          item,
-          ...rest
-        }),
+        (acc, item) => reduceChildRoutes({ acc, item, ...rest }),
         []
       )}
-    </List>
+    </ul>
   )
 }
 
-function reduceChildRoutes ({
-  acc,
-  pathname,
-  item,
-  depth = 0
-}) {
+function reduceChildRoutes ({ acc, pathname, item, depth = 0 }) {
   const key = item.title + depth
 
   if (item.items) {
@@ -80,125 +64,80 @@ function reduceChildRoutes ({
   return acc
 }
 
-const useStyles = makeStyles(() => ({
-  mobileDrawer: {
-    width: 256
-  },
-  desktopDrawer: {
-    width: 236,
-    top: 64,
-    height: 'calc(100% - 64px)'
-  },
-  avatar: {
-    cursor: 'pointer',
-    width: 64,
-    height: 64
-  }
-}))
-
-const NavBar = ({
-  openMobile,
-  onMobileClose
-}) => {
-  const classes = useStyles()
+const NavBar = ({ openMobile, onMobileClose }) => {
   const location = useLocation()
+  const [isOpen, setIsOpen] = useState(openMobile)
 
   useEffect(() => {
-    if (openMobile && onMobileClose) onMobileClose()
+    setIsOpen(openMobile)
+  }, [openMobile])
 
-    // eslint-disable-next-line
-  }, [location.pathname]);
+  useEffect(() => {
+    if (isOpen && onMobileClose) {
+      onMobileClose()
+    }
+  }, [location.pathname])
+
+  const handleOpenChange = (open) => {
+    setIsOpen(open)
+    if (!open && onMobileClose) {
+      onMobileClose()
+    }
+  }
 
   const content = (
-    <Box
-      height='100%'
-      display='flex'
-      flexDirection='column'
-    >
-      <PerfectScrollbar options={{ suppressScrollX: true }}>
-        <Hidden lgUp>
-          <Box
-            p={2}
-            display='flex'
-            justifyContent='center'
-          >
-            <RouterLink to='/'>
-              <Logo />
-            </RouterLink>
-          </Box>
-        </Hidden>
-        <Box p={2}>
-          <Box
-            p={2}
-            borderRadius={1}
-            bgcolor='background.dark'
-          >
-            <Typography
-              variant='h6'
-              color='textPrimary'
-            >
-              {format.dayOfWeek(new Date())}
-              {', '}
-              {format.date(new Date())}
+    <div className='flex flex-col h-full'>
+      <ScrollArea className='flex-grow'>
+        <div className='lg:hidden p-4 flex justify-center'>
+          <RouterLink to='/'>
+            <Logo />
+          </RouterLink>
+        </div>
+        <div className='p-4'>
+          <div className='p-4 rounded bg-gray-100 dark:bg-gray-800'>
+            <Typography variant='h6' className='font-semibold'>
+              {format.dayOfWeek(new Date())}, {format.date(new Date())}
             </Typography>
-            <Typography
-              variant='h6'
-              color='textPrimary'
-            >
-              Semana
-              {' '}
-              {format.weekOfYear(new Date())}
+            <Typography variant='h6' className='font-semibold'>
+              Semana {format.weekOfYear(new Date())}
             </Typography>
-          </Box>
-        </Box>
-        <hr />
-        <Box p={2}>
-          {navConfig.map(config => (
-            <List
-              key={uniqId()}
-              subheader={(
-                <ListSubheader
-                  disableGutters
-                  disableSticky
-                >
-                  {config.subheader}
-                </ListSubheader>
-              )}
-            >
+          </div>
+        </div>
+        <hr className='border-t border-gray-200 dark:border-gray-700' />
+        <div className='p-4'>
+          {navConfig.map((config) => (
+            <div key={uniqId()} className='mb-4'>
+              <Typography variant='h6' className='mb-1 font-semibold text-gray-500 dark:text-gray-400'>
+                {config.subheader}
+              </Typography>
               {renderNavItems({
                 items: config.items,
                 pathname: location.pathname
               })}
-            </List>
+            </div>
           ))}
-        </Box>
-      </PerfectScrollbar>
-    </Box>
+        </div>
+      </ScrollArea>
+    </div>
   )
 
   return (
     <>
-      <Hidden lgUp>
-        <Drawer
-          anchor='left'
-          classes={{ paper: classes.mobileDrawer }}
-          onClose={onMobileClose}
-          open={openMobile}
-          variant='temporary'
-        >
+      <Sheet open={isOpen} onOpenChange={handleOpenChange}>
+        <SheetTrigger asChild>
+          <Button variant='outline' size='icon' className='lg:hidden'>
+            <Menu className='h-4 w-4' />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side='left' className='w-64 p-0 z-[2100]'>
           {content}
-        </Drawer>
-      </Hidden>
-      <Hidden lgDown>
-        <Drawer
-          anchor='left'
-          classes={{ paper: classes.desktopDrawer }}
-          open
-          variant='persistent'
-        >
-          {content}
-        </Drawer>
-      </Hidden>
+        </SheetContent>
+      </Sheet>
+      <div
+        className='hidden lg:block w-60 fixed top-16 left-0 bottom-0 border-r border-gray-200 dark:border-gray-800 bg-popover'
+      >
+        {content}
+      </div>
     </>
   )
 }
