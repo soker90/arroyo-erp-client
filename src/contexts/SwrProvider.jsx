42218ea091648da30ff22d4c3addb2faf49c1613
Eduardo Parra <eduparra90@gmail.com>
2024-11-06 23:57:26 +0100

    remove swr with localstorage

--- src/contexts/SwrProvider.jsx
+++ src/contexts/SwrProvider.jsx
@@ -1,10 +1,9 @@
 import { SWRConfig } from 'swr'
 import axios from 'axios'
-import { localStorageProvider } from '../utils/localStorageProvider'
 
 const SwrProvider = ({ children }) => {
   return (
-    <SWRConfig value={{ provider: localStorageProvider, fetcher: (url) => axios.get(url).then(res => res.data), refreshInterval: 0 }}>
+    <SWRConfig value={{ fetcher: (url) => axios.get(url).then(res => res.data), refreshInterval: 0 }}>
       {children}
     </SWRConfig>
   )
